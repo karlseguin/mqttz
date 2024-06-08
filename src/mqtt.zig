d694bcf889c702b7b72a1667b0fe0a320015117e
Karl Seguin <k@openmymind.io>
2024-06-08 20:01:15 +0800

    merge packet logic directly in mqtt file

--- src/mqtt.zig
+++ src/mqtt.zig
@@ -1,6 +1,6 @@
 const codec = @import("codec.zig");
-const packet = @import("packet.zig");
 const properties = @import("properties.zig");
+const PropertyReader = properties.Reader;
 
 const MIN_BUF_SIZE = 256;
 
@@ -300,7 +300,7 @@ pub fn Mqtt(comptime S: type) type {
 			MalformedPacket,
 			Response,
 		};
-		pub fn readPacket(self: *Self, state: anytype) ReadError!packet.Packet {
+		pub fn readPacket(self: *Self, state: anytype) ReadError!Packet {
 			const p = try self.readOrBuffered(state);
 			switch (p) {
 				.connack => |*connack| try self.processConnack(state, connack),
@@ -309,7 +309,7 @@ pub fn Mqtt(comptime S: type) type {
 			return p;
 		}
 
-		fn readOrBuffered(self: *Self, state: anytype) !packet.Packet {
+		fn readOrBuffered(self: *Self, state: anytype) !Packet {
 			if (try self.bufferedPacket()) |p| {
 				return p;
 			}
@@ -371,7 +371,7 @@ pub fn Mqtt(comptime S: type) type {
 		}
 
 		// see if we have a full packet in our read_buf already
-		fn bufferedPacket(self: *Self) !?packet.Packet {
+		fn bufferedPacket(self: *Self) !?Packet {
 			const buf = self.read_buf[self.read_pos..self.read_len];
 
 			// always has to be at least 2 bytes
@@ -397,7 +397,7 @@ pub fn Mqtt(comptime S: type) type {
 			}
 
 			self.read_pos += total_len;
-			return packet.parse(buf[0], buf[fixed_header_len..total_len]) catch |err| {
+			return decodePacket(buf[0], buf[fixed_header_len..total_len]) catch |err| {
 				self.last_error = .{.inner = err};
 				switch (err) {
 					error.UnknownPacketType => return error.Protocol,
@@ -406,7 +406,7 @@ pub fn Mqtt(comptime S: type) type {
 			};
 		}
 
-		fn processConnack(self: *Self, state: anytype, connack: *const packet.ConnAck) !void {
+		fn processConnack(self: *Self, state: anytype, connack: *const Packet.ConnAck) !void {
 			switch (connack.reason_code) {
 				0 => {}, // success
 				1...127 => return self.receivedInvalidReason(state),        // returns an error.Protocol
@@ -537,7 +537,7 @@ fn encodeConnect(buf: []u8, opts: ConnectOpts) ![]u8 {
 	if (opts.password) |p| {
 		pos += try codec.writeString(buf[pos..], p);
 	}
-	return finalizePacket(buf[0..pos], 1, 0);
+	return encodePacketHeader(buf[0..pos], 1, 0);
 }
 
 fn encodeDisconnect(buf: []u8, opts: DisconnectOpts) ![]u8 {
@@ -547,7 +547,7 @@ fn encodeDisconnect(buf: []u8, opts: DisconnectOpts) ![]u8 {
 	const PROPERTIES_OFFSET = 6;
 	const properties_len = try properties.write(buf[PROPERTIES_OFFSET..], opts, &properties.DISCONNECT);
 
-	return finalizePacket(buf[0..PROPERTIES_OFFSET + properties_len], 14, 0);
+	return encodePacketHeader(buf[0..PROPERTIES_OFFSET + properties_len], 14, 0);
 }
 
 fn encodeSubscribe(buf: []u8, packet_identifier: u16, opts: SubscribeOpts) ![]u8 {
@@ -579,7 +579,7 @@ fn encodeSubscribe(buf: []u8, packet_identifier: u16, opts: SubscribeOpts) ![]u8
 		pos += 1;
 	}
 
-	return finalizePacket(buf[0..pos], 8, 2);
+	return encodePacketHeader(buf[0..pos], 8, 2);
 }
 
 fn encodePublish(buf: []u8, packet_identifier: u16, opts: PublishOpts) ![]u8 {
@@ -606,11 +606,10 @@ fn encodePublish(buf: []u8, packet_identifier: u16, opts: PublishOpts) ![]u8 {
 	const payload_offset = properties_offset + properties_len;
 	const payload_len = try codec.writeString(buf[payload_offset..], opts.message);
 
-	return finalizePacket(buf[0..payload_offset + payload_len], 3, @as([*]u4, @ptrCast(@alignCast(&publish_flags)))[0]);
+	return encodePacketHeader(buf[0..payload_offset + payload_len], 3, @as([*]u4, @ptrCast(@alignCast(&publish_flags)))[0]);
 }
 
-fn finalizePacket(buf: []u8, packet_type: u8, packet_flags: u8) []u8 {
-
+fn encodePacketHeader(buf: []u8, packet_type: u8, packet_flags: u8) []u8 {
 	const remaining_len = buf.len - 5;
 	const length_of_len = codec.lengthOfVarint(remaining_len);
 
@@ -623,6 +622,180 @@ fn finalizePacket(buf: []u8, packet_type: u8, packet_flags: u8) []u8 {
 	_ = codec.writeVarint(buf[start+1..], remaining_len);
 	return buf[start..];
 }
+
+// These are only the packets that a client can receive
+// For packets that the client sends, we write the Opts (e.g. ConnectionOpts)
+// directly to the write_buf
+pub const Packet = union(enum) {
+	connack: ConnAck,
+	suback: SubAck,
+
+	pub const ConnAck = struct {
+		session_present: bool,
+		reason_code: u8,
+		session_expiry_interval: ?u32 = null,
+		receive_maximum: ?u16 = null,
+		maximum_qos: ?QoS = null,
+		retain_available: ?bool = null,
+		maximum_packet_size: ?u32 = null,
+		assigned_client_identifier: ?[]const u8 = null,
+		topic_alias_maximum: ?u16 = null,
+		reason_string: ?[]const u8 = null,
+		wildcard_subscription_available: ?bool = null,
+		subscription_identifier_available: ?bool = null,
+		shared_subscription_available: ?bool = null,
+		server_keepalive: ?u16 = null,
+		response_information: ?[]const u8 = null,
+		server_reference: ?[]const u8 = null,
+		authentication_method: ?[]const u8 = null,
+		authentication_data: ?[]const u8 = null,
+	};
+
+	pub const SubAck = struct {
+		packet_identifier: u16,
+		reason_string: ?[]const u8 = null,
+		results: []const u8,
+
+		const Result = union(enum) {
+			granted: QoS,
+			err: Error,
+			invalid_index: void,
+
+			const Error = enum {
+				unspecified_error,
+				implementation_specific_error,
+				not_authorized,
+				topic_filter_invalid,
+				packet_identifier_in_use,
+				quota_exceeded,
+				shared_subscriptions_not_supported,
+				subscription_identifier_not_supported,
+				wildcard_subscriptions_not_supported,
+				unknown,
+			};
+		};
+
+		pub fn result(self: *const SubAck, topic_index: usize) Result {
+				const results = self.results;
+
+				if (topic_index > results.len) {
+					return .{.invalid_index = {}};
+				}
+
+				switch (results[topic_index]) {
+					0 => return .{.granted = .at_most_once},
+					1 => return .{.granted = .at_least_once},
+					2 => return .{.granted = .exactly_once},
+					128 => return .{.err = .unspecified_error},
+					131 => return .{.err = .implementation_specific_error},
+					135 => return .{.err = .not_authorized},
+					143 => return .{.err = .topic_filter_invalid},
+					145 => return .{.err = .packet_identifier_in_use},
+					151 => return .{.err = .quota_exceeded},
+					158 => return .{.err = .shared_subscriptions_not_supported},
+					161 => return .{.err = .subscription_identifier_not_supported},
+					162 => return .{.err = .wildcard_subscriptions_not_supported},
+					else => return .{.err = .unknown},
+				}
+			}
+	};
+};
+
+pub fn decodePacket(b1: u8, data: []u8) !Packet {
+	// data.len has to be > 0
+	// TODO: how to assert without std?
+	const flags = b1 & 15;
+	switch (b1 >> 4) {
+		2 => return .{.connack = try decodeConnAck(data, flags)},
+		9 => return .{.suback = try decodeSubAck(data, flags)},
+		else => return error.UnknownPacketType, // TODO
+	}
+}
+
+pub fn decodeConnAck(data: []u8, flags: u8) !Packet.ConnAck {
+	if (flags != 0) {
+		return error.InvalidFlags;
+	}
+
+	if (data.len < 3) {
+		// must have at least 3 bytes
+		// (ConnAct flag, Reason Code, Property Length)
+		return error.IncompletePacket;
+	}
+
+	const session_present = switch (data[0]) {
+		0 => false,
+		1 => true,
+		else => return error.MalformedPacket,
+	};
+
+	var connack = Packet.ConnAck{
+		.session_present = session_present,
+		.reason_code = data[1],
+	};
+
+	var props = try PropertyReader.init(data[2..]);
+	while (try props.next()) |prop| {
+		switch (prop) {
+			.session_expiry_interval => |v| connack.session_expiry_interval = v,
+			.receive_maximum => |v| connack.receive_maximum = v,
+			.maximum_qos => |v| connack.maximum_qos = v,
+			.retain_available => |v| connack.retain_available = v,
+			.maximum_packet_size => |v| connack.maximum_packet_size = v,
+			.assigned_client_identifier => |v| connack.assigned_client_identifier = v,
+			.topic_alias_maximum => |v| connack.topic_alias_maximum = v,
+			.reason_string => |v| connack.reason_string = v,
+			.wildcard_subscription_available => |v| connack.wildcard_subscription_available = v,
+			.subscription_identifier_available => |v| connack.subscription_identifier_available = v,
+			.shared_subscription_available => |v| connack.shared_subscription_available = v,
+			.server_keepalive => |v| connack.server_keepalive = v,
+			.response_information => |v| connack.response_information = v,
+			.server_reference => |v| connack.server_reference = v,
+			.authentication_method => |v| connack.authentication_method = v,
+			.authentication_data => |v| connack.authentication_data = v,
+			.user_property => {}, // TODO: handle
+			else => return error.InvalidProperty,
+		}
+	}
+
+	return connack;
+}
+
+fn decodeSubAck(data: []u8, flags: u8) !Packet.SubAck {
+	if (flags != 0) {
+		return error.InvalidFlags;
+	}
+
+	if (data.len < 4) {
+		// must have at least 4 bytes
+		// 2 for the packet identifier
+		// at least 1 for a 0-length property list
+		// at least 1 for 1 reason code in the body
+		return error.IncompletePacket;
+	}
+
+	var suback = Packet.SubAck{
+		.packet_identifier = codec.readInt(u16, data[0..2]),
+		.results = undefined,
+	};
+
+	var props = try PropertyReader.init(data[2..]);
+	while (try props.next()) |prop| {
+		switch (prop) {
+			.reason_string => |v| suback.reason_string = v,
+			.user_property => {}, // TODO: handle
+			else => return error.InvalidProperty,
+		}
+	}
+
+	// the rest of the packet is the payload, and the payload is 1 or more
+	// 1 byte reason codes. 1 reason code per subscribed topic (in the same order)
+	// So if you subscribed to 2 topics and want to know the result of the 2nd one
+	// you would check suback.results[1].
+	suback.results = data[2 + props.len..];
+	return suback;
+}
+
 
 const t = @import("std").testing;
 
